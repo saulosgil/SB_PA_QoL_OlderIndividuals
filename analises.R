22e41bfa2b118caa5504b4b5486a20adf5d8f060
saulosgil@hotmail.com <saulosgil@hotmail.com>
2026-05-18 15:23:08 -0300

    update: figure function

--- analises.R
+++ analises.R
@@ -10,21 +10,23 @@ df <-
 
 # analises exploratorias ----------------------------------------------------------------------
 
-figura <- function(df, x_var, y_var){
-  ggplot(df, aes(x = .data[[x_var]],
-                 y = .data[[y_var]])) +
+figura <- function(df, x_var, y_var, diretorio = "."){
+  plot <- ggplot(df, aes(x = .data[[x_var]],
+                         y = .data[[y_var]])) +
     geom_point() +
     geom_smooth(method = "lm")
-}
 
+nome_arquivo <- file.path(diretorio, paste0("figura_", x_var, "_", y_var, ".png"))
+ggsave(nome_arquivo, plot = plot, width = 8, height = 6, dpi = 300)
+
+  plot
+}
 
 variaveis <- c("ansiedade_score", "depressao_score")
 
 for (v in variaveis) {
   print(figura(df, "mvpa_minday", v))
 }
-
-
 
 colnames(df)
 
