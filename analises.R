f4ebe5cda3a4e85520b625cefae0c44aeb7fb24d
saulosgil@hotmail.com <saulosgil@hotmail.com>
2026-05-22 17:11:51 -0300

    update- analises exploratorias

--- analises.R
+++ analises.R
@@ -11,17 +11,62 @@ df <-
   filter(idade >= 60)
 
 colnames(df)
-readr::write_csv(df, "data_exploratory.csv")
+
+# Checking outcomes ---------------------------------------------------------------------------
+# ajustado
+df <- 
+  df |> 
+  mutate(
+    hgs_max = if_else(hgs_max > 99, hgs_max/100, hgs_max),
+    hgs_max = if_else(hgs_max < 5, mean(hgs_max), hgs_max)
+  ) |>
+  mutate(
+    imc = imc/10,
+    imc = if_else(imc < 20, mean(imc), imc)
+  ) |> 
+  filter(ts_max > 3 & ts_max < 30) |> 
+  select(-tug_max)
 
 # Missing  ----------------------------------------------------------------------
 plot_missing(df)
+
+# variáveis que irão no eixo Y
+variaveis <- c("hgs_max", 
+               "ts_max", 
+               "ansiedade_score",
+               "depressao_score",
+               "whoqol_fisico_escore_100",
+               "whoqol_psicol_escore_100",
+               "whoqol_social_escore_100",
+               "whoqol_ambiente_escore_100")
+
+
+for (y_var in variaveis) {
+  
+  # fórmula da regressão
+  formula <- as.formula(paste(y_var, "~ total_sb_hday"))
+  
+  # gráfico
+  plot(df$total_sb_hday,
+       df[[y_var]],
+       main = paste("Regressão Linear -", y_var),
+       xlab = "total_sb_hday",
+       ylab = y_var,
+       pch = 19)
+  
+  # modelo
+  modelo <- lm(formula, data = df)
+  
+  # linha de regressão
+  abline(modelo,
+         col = "red",
+         lwd = 2)
+}
 
 # SB categories -------------------------------------------------------------------------------
 df <- 
   df |> 
   mutate(
-    mpsb_higher_6h = if_else(total_sb_mp_hday >= 6, "highermpsb", "lowermpsb"),
-    masb_higher_6h = if_else(total_sb_ma_hday >= 6, "highermpsb", "lowermpsb"),
     sb_higher_6h = if_else(total_sb_hday >= 6, "highersb", "lowersb")
   )
 
@@ -30,67 +75,50 @@ df <-
 df |> 
   count(sb_higher_6h)
 
-# MPSB
-df |> 
-  count(mpsb_higher_6h)
-
-# MSSB
-df |> 
-  count(masb_higher_6h)
-
 # analyses ------------------------------------------------------------------------------------
 # HGS -----------------------------------------------------------------------------------------
 df |> 
-  tidyplot(x = mpsb_higher_6h, 
+  tidyplot(x = sb_higher_6h, 
            y = hgs_max, 
-           color = mpsb_higher_6h) |> 
-  add_boxplot() |> 
-  add_median_dot()
+           color = sb_higher_6h) |> 
+  add_boxplot(show_outliers = FALSE) |> 
+  add_mean_dot()
 
-testt <- t.test(hgs_max ~ mpsb_higher_6h, df)
-testt
-
-# TUG -----------------------------------------------------------------------------------------
-df |> 
-  tidyplot(x = quartil_sedentario, 
-           y = tug_max, 
-           color = quartil_sedentario) |> 
-  add_boxplot() |> 
-  add_median_dot()
-
-testt <- t.test(tug_max ~ quartil_sedentario, df)
+testt <- t.test(hgs_max ~ sb_higher_6h, df)
 testt
 
 # TS -----------------------------------------------------------------------------------------
 df |> 
-  tidyplot(x = mpsb_higher_6h, 
+  tidyplot(x = sb_higher_6h, 
            y = ts_max, 
-           color = mpsb_higher_6h) |> 
-  add_boxplot() |> 
-  add_median_dot()
+           color = sb_higher_6h) |> 
+  add_boxplot(show_outliers = FALSE) |> 
+  add_mean_dot()
 
-testt <- t.test(ts_max ~ mpsb_higher_6h, df)
+
+testt <- t.test(ts_max ~ sb_higher_6h, df)
 testt
 
 # depression -----------------------------------------------------------------------------------------
 df |> 
-  tidyplot(x = mpsb_higher_6h, 
+  tidyplot(x = sb_higher_6h, 
            y = depressao_score, 
-           color = mpsb_higher_6h) |> 
-  add_boxplot() |> 
-  add_median_dot()
+           color = sb_higher_6h) |> 
+  add_boxplot(show_outliers = FALSE) |> 
+  add_mean_dot()
 
-testt <- t.test(depressao_score ~ mpsb_higher_6h, df)
+testt <- t.test(depressao_score ~ sb_higher_6h, df)
 testt
 
 # anxiety -----------------------------------------------------------------------------------------
 df |> 
-  tidyplot(x = mpsb_higher_6h, 
+  tidyplot(x = sb_higher_6h, 
            y = ansiedade_score, 
-           color = mpsb_higher_6h) |> 
-  add_boxplot()
+           color = sb_higher_6h) |> 
+  add_boxplot(show_outliers = FALSE) |> 
+  add_mean_dot()
 
-testt <- t.test(ansiedade_score ~ mpsb_higher_6h, df)
+testt <- t.test(ansiedade_score ~ sb_higher_6h, df)
 testt
 
 # WHO - fisico -----------------------------------------------------------------------------------------
@@ -98,8 +126,8 @@ df |>
   tidyplot(x = sb_higher_6h, 
            y = whoqol_fisico_escore_100, 
            color = sb_higher_6h) |> 
-  add_boxplot() |> 
-  add_median_dot()
+  add_boxplot(show_outliers = FALSE) |> 
+  add_mean_dot()
 
 testt <- t.test(whoqol_fisico_escore_100 ~ sb_higher_6h, df)
 testt
@@ -109,44 +137,63 @@ df |>
   tidyplot(x = sb_higher_6h, 
            y = whoqol_psicol_escore_100, 
            color = sb_higher_6h) |> 
-  add_boxplot() |> 
-  add_median_dot()
+  add_boxplot(show_outliers = FALSE) |> 
+  add_mean_dot()
 
 testt <- t.test(whoqol_psicol_escore_100 ~ sb_higher_6h, df)
 testt
 
 # WHO - social -----------------------------------------------------------------------------------------
 df |> 
-  tidyplot(x = mpsb_higher_6h, 
+  tidyplot(x = sb_higher_6h, 
            y = whoqol_social_escore_100, 
-           color = mpsb_higher_6h) |> 
-  add_boxplot() |> 
-  add_median_dot()
+           color = sb_higher_6h) |> 
+  add_boxplot(show_outliers = FALSE) |> 
+  add_mean_dot()
 
-testt <- t.test(whoqol_social_escore_100 ~ mpsb_higher_6h, df)
+testt <- t.test(whoqol_social_escore_100 ~ sb_higher_6h, df)
 testt
 
 # WHO - ambiente -----------------------------------------------------------------------------------------
 df |> 
-  tidyplot(x = mpsb_higher_6h, 
+  tidyplot(x = sb_higher_6h, 
            y = whoqol_ambiente_escore_100, 
-           color = mpsb_higher_6h) |> 
-  add_boxplot() |> 
-  add_median_dot()
+           color = sb_higher_6h) |> 
+  add_boxplot(show_outliers = FALSE) |> 
+  add_mean_dot()
 
-testt <- t.test(whoqol_ambiente_escore_100 ~ mpsb_higher_6h, df)
+testt <- t.test(whoqol_ambiente_escore_100 ~ sb_higher_6h, df)
 testt
 
-df <- df |> 
-  mutate(
-    quartil_sedentario = ntile(total_sb_hday, 3),
-    quartil_sedentario = factor(
-      quartil_sedentario,
-      levels = 1:3,
-      labels = c("Q1 - Menor tempo",
-                 "Q2",
-                 "Q3")
-    )
-  )
+# Regression with significant outcome -------------------------------------------
+# ----------
+# Total SB 
+# ----------
+summary(lm(depressao_score ~ df$total_sb_hday,data = df))
 
-df$quartil_sedentario
+# ----------
+# MPSB 
+# ----------
+# Napping
+# Listening to music
+# Watching television
+# Talking using or not a smartphone when sited
+# Sit in car, bus or train
+# Sit in the church or the theater
+
+summary(lm(depressao_score ~ df$total_soneca_minday,data = df))
+
+summary(lm(depressao_score ~ df$total_musica_minday,data = df))
+
+summary(lm(depressao_score ~ df$total_tv_minday,data = df))
+
+summary(lm(depressao_score ~ df$total_telefone_minday,data = df))
+
+summary(lm(depressao_score ~ df$total_transporte_minday,data = df))
+
+summary(lm(depressao_score ~ df$total_atvculturais_minday,data = df))
+
+
+
+
+
