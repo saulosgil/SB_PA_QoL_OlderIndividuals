0c26b2d318d766ee06c32d61b1ac8b2bd2388c10
saulosgil@hotmail.com <saulosgil@hotmail.com>
2026-05-25 19:33:10 -0300

    update: categorizando os MPSB pela mediana

--- analises.R
+++ analises.R
@@ -118,8 +118,55 @@ library(patchwork)
 dep/anx
 
 # plots of each MPSB and depression -----------------------------------------------------------
-# Fazer os plot de cada dominio de SB (categorizar >=4h/day)
+# Fazer os plot de cada dominio de SB (categorizar pela mediana)
+
+df <- 
+  df |> 
+  mutate(
+    napping_mediana = ntile(total_soneca_minday,n = 2),
+    napping_mediana = factor(
+      napping_mediana,
+      levels = 1:2,
+      labels = c("Lower", "Higher")
+    )
+  ) |> 
+  mutate(
+    
+  ) |> 
+  mutate(
+    
+  ) |> 
+  mutate(
+    
+  ) |> 
+  mutate(
+    
+  ) |> 
+  mutate(
+    
+  )
+
 # Napping
+napping <- 
+  df |> 
+  tidyplot(x = napping_mediana, 
+           y = depressao_score, 
+           color = napping_mediana) |> 
+  add_boxplot(show_outliers = FALSE) |> 
+  add_mean_dot()|> 
+  add_mean_dot() +
+  labs(
+    y = "Beck Depression Inventory Score (a.u.)",
+  ) +
+  theme(axis.title.x = element_blank(),
+        legend.title = element_blank()) +
+  scale_y_continuous(breaks = seq(0, 30, 10)) +
+  coord_cartesian(ylim = c(0, 30))
+
+napping
+
+testt <- t.test(depressao_score ~ napping_mediana, df)
+testt
 # Listening to music
 # Watching television
 # Talking using or not a smartphone when sited
